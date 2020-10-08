e174447fc2e76ebeeb936d8dd8aacc10ee8fc192
datisp <63231965+datisp@users.noreply.github.com>
2020-10-08 16:34:34 +0200

    Update trimming.sh

--- trimming.sh
+++ trimming.sh
@@ -11,20 +11,20 @@ mkdir reads_trimmed
 
 # copy raw reads
 echo "copying raw data..."
-#cp $path_raw_data/*$var_strain1*fq.gz ./reads_raw/
-#cp $path_raw_data/*$var_strain2*fq.gz ./reads_raw/
+cp $path_raw_data/*$var_strain1*fq.gz ./reads_raw/
+cp $path_raw_data/*$var_strain2*fq.gz ./reads_raw/
 
-#cp $path_raw_data/{$strain1_rep1$filetype$compression,$strain1_rep2$filetype$compression,$strain1_rep3$filetype$compression,$strain2_rep1$filetype$compression,$strain2_rep2$filetype$compression,$strain2_rep3$filetype$compression} ./reads_raw/
+cp $path_raw_data/{$strain1_rep1$filetype$compression,$strain1_rep2$filetype$compression,$strain1_rep3$filetype$compression,$strain2_rep1$filetype$compression,$strain2_rep2$filetype$compression,$strain2_rep3$filetype$compression} ./reads_raw/
 
 # perform FastQC analysis for unprocessed reads
 echo "performing FastQC analysis for raw reads..."
-#$path_fastqc/fastqc ./reads_raw/*$filetype$compression
+$path_fastqc/fastqc ./reads_raw/*$filetype$compression
 
 # activate conda for MultiQC analysis
 source activate $conda_multiqc
 
 # perform MultiQC analysis for raw reads
-#multiqc ./reads_raw/*fastqc.zip -o ./reads_raw/
+multiqc ./reads_raw/*fastqc.zip -o ./reads_raw/
 
 # activate conda trimming for trim-galore
 echo "removal of adapter sequences and quality trimming..." 
